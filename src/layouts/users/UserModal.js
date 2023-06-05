730a967d3e2655fbdd4bf8ba052d94f18a425ed5
aizaz-react <aizaz9790@gmail.com>
2023-06-05 23:42:23 +0500

    first commit

--- src/layouts/users/UserModal.js
+++ src/layouts/users/UserModal.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import PropTypes from "prop-types";
